3eaf9b68bbe71f22f6a31e12117b35e44bb0b059
jns-w <jns.w@icloud.com>
2025-08-09 23:51:46 +0800

    add vim cmd to enable extended symbols

--- lua/custom/plugins/flog.lua
+++ lua/custom/plugins/flog.lua
@@ -2,12 +2,13 @@
 -- flog displays git branches and commit flows
 
 vim.keymap.set('n', '<leader>fl', '<Cmd>Flogsplit<CR>')
+vim.g.flog_enable_extended_chars = 1
 
 return {
-	"rbong/vim-flog",
-	lazy = true,
-	cmd = { "Flog", "Flogsplit", "Floggit" },
-	dependencies = {
-		"tpope/vim-fugitive",
-	},
+  'rbong/vim-flog',
+  lazy = true,
+  cmd = { 'Flog', 'Flogsplit', 'Floggit' },
+  dependencies = {
+    'tpope/vim-fugitive',
+  },
 }
