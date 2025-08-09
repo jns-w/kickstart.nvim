e57d8a24db26d2b2d4ed82018e5ee9afffd10e4a
jns-w <jns.w@icloud.com>
2025-08-09 21:19:09 +0800

    add vim-flog

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	"rbong/vim-flog",
+	lazy = true,
+	cmd = { "Flog", "Flogsplit", "Floggit" },
+	dependencies = {
+		"tpope/vim-fugitive",
+	},
+}
