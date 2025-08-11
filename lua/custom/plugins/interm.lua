0e4cc4d19edf5425a3b6b3dd79ea6034211b9f0f
jns-w <jns.w@icloud.com>
2025-08-11 11:31:53 +0800

    fix: toggling existing interm buffer now also goes into insert mode immediately

--- lua/custom/plugins/interm.lua
+++ lua/custom/plugins/interm.lua
@@ -47,7 +47,9 @@ local toggle_terminal = function()
     state.floating = create_floating_window { buf = state.floating.buf }
     if vim.bo[state.floating.buf].buftype ~= 'terminal' then
       vim.cmd.terminal()
-      vim.cmd 'startinsert'
+      vim.cmd 'startinsert' -- start terminal on insert mode
+    else
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-\\><C-n>i', true, false, true), 'n', true) -- go into insert mode when toggling existing terminal buffer
     end
   else
     vim.api.nvim_win_hide(state.floating.win)
